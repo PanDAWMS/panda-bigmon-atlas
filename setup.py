aa50eabe329194dfaeebf0133299c39413db7cc0
jschovan <jschovan@gmail.com>
2014-04-24 20:21:48 +0000

    Merged devel/bigpandamon-atlas to trunk/bigpandamon-atlas, RC for bigpandamon-atlas-0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Setup prog for Panda Server
 #
 #
-release_version = '0.0.5' #FIXME
+release_version = '0.0.6' #FIXME
 prefix = '/data/atlpan/bigpandamon'
 lib_prefix = 'lib/python2.6/site-packages/'
 expected_extensions = ['.html', '.js', '.css', '.png', '.gif', '.ico', '-example'] #FIXME
