908c94bc128ba80bf7aa84dc67b892ce1bc9e98f
jschovan <jschovan@gmail.com>
2014-05-19 18:17:05 +0000

    Preparing for continuous RPM build.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 #
-# Setup prog for Panda Server
+# Setup prog for bigpandamon-atlas
 #
 #
-release_version = '0.0.7' #FIXME
-prefix = '/data/atlpan/bigpandamon'
+from version import __version__, __provides__
+#prefix = '/data/atlpan/bigpandamon'
+prefix = '/data/wenaus/bigpandamon'
 lib_prefix = 'lib/python2.6/site-packages/'
 expected_extensions = ['.html', '.js', '.css', '.png', '.gif', '.ico', '-example'] #FIXME
+src_ext = [ '.py' ]
+ignore_dir = [ '/.svn', '/.settings' ]
 
 import os
 import re
@@ -91,7 +94,14 @@ class install_data_panda (install_data_org):
             if not res and filename.endswith(ext):
                 res = True
         return res
-    
+    def is_src_extension(self, filename):
+        res = False
+        for ext in src_ext:
+            if not res and filename.endswith(ext):
+                res = True
+        return res
+
+
     def run (self):
         # remove /usr for bdist/bdist_rpm
         match = re.search('(build/[^/]+/dumb)/usr',self.install_dir)
@@ -164,8 +174,8 @@ def gen_data_files(*dirs):
 
 # setup for distutils
 setup(
-    name="bigpandamon-atlas",
-    version=release_version,
+    name=__provides__,
+    version=__version__,
     description='BigPanDA Monitoring Package - ATLAS',
     long_description='''This package contains BigPanDA Monitoring Components - ATLAS''',
     license='GPL',
@@ -180,6 +190,7 @@ setup(
         'atlas.postproduction.deft',
         'atlas.prodtask',
         'atlas.todoview',
+	'atlas.getdatasets',
     ],
     data_files=[ #FIXME
                 # config files 
@@ -188,12 +199,14 @@ setup(
                  ),
                 # HTML templates and static files
                 ]
-                 + gen_data_files("atlas/templates",
-                                  "atlas/static",
-                                  "atlas/media",
-                                  "atlas/config-templates",
-                                  "atlas/prodtask/templates",
-                                  "atlas/todoview/templates",
+                 + gen_data_files(
+		     "atlas/templates",
+                     "atlas/static",
+                     "atlas/media",
+                     "atlas/config-templates",
+                     "atlas/prodtask/templates",
+                     "atlas/todoview/templates",
+                     "atlas/getdatasets/templates",
                 )
     ,         
     cmdclass={'install': install_panda,
