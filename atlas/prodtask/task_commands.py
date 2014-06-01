c391a55b68d3ed44138e27ae3dc7273b6211281f
sbel <belov.sd@gmail.com>
2014-06-01 17:01:37 +0000

    Merged from 'branches/sbelov/sbelov_task_manage_v2'

--- atlas/prodtask/task_commands.py
+++ atlas/prodtask/task_commands.py
@@ -3,27 +3,74 @@ Temporary placeholder to exec Jedi client remotely.
 To be replaced with upcoming DEFT API functions
 """
 
+import ast
 import os.path
+import re
 import subprocess
 import atlas.settings
 
-rsa_key_file = "%s/%s" %(os.path.dirname(os.path.abspath(atlas.settings.__file__)), "ssh/id_rsa")
+rsa_key_file = "%s/%s" %(os.path.dirname(os.path.abspath(atlas.settings.__file__)), "jediclient-ssh/id_rsa")
+
+def _execJediCommand(task_id, command, *params):
+    jedi_commands = ['killTask', 'changeTaskPriority', 'reassignTaskToSite', 'reassignTaskToCloud']
+
+    if not command in jedi_commands:
+        raise ValueError( "JEDI command not supported: '%s'" % (command) )
+
+    (task_id, params) = (str(task_id), [str(x) for x in params])
+    action_call = "import jedi.client as jc; print jc.%s(%s)" % ( command, ",".join(list([task_id])+params) )
+
+    p = subprocess.Popen(['ssh', '-q',
+                          '-i', rsa_key_file,
+                          '-o', 'StrictHostKeyChecking=no',
+                          '-o', 'UserKnownHostsFile=/dev/null',
+                          '-o', 'LogLevel=QUIET',
+                          'sbelov@aipanda015',
+                          'PYTHONPATH=/mnt/atlswing/site-packages/ python -c "%s"' % (action_call),
+                          '2>/dev/null'
+                         ],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = p.communicate()
+    bash_lines = re.compile("^bash:")
+    # remove shell warnings if any
+    out = [x.rstrip() for x in out if not bash_lines.match(x)]
+
+
+    result = {}
+    if not out: return result
+    jedi_response = tuple(ast.literal_eval(out[0]))
+    if not jedi_response: return result
+
+    (accepted, registered, message) = (False, False, '')
+    (status_code, return_code) = (0, 0)
+
+    status_code = jedi_response[0]
+    if  status_code == 0:
+        accepted = True
+
+        if command == "changeTaskPriority":
+            return_code = jedi_response[1]
+        else:
+            (return_code, message) = jedi_response[1]
+        registered = bool(return_code)
+        result['jedi_return_code'] = return_code
+    else:
+        message = jedi_response[1]
+
+    result.update(accepted=accepted, registered=registered, jedi_message=message, jedi_status_code=status_code)
+
+    return result
+
 
 def killTask(task_id):
-    p = subprocess.Popen(["ssh", "-i", rsa_key_file, "sbelov@aipanda015",
-                          'PYTHONPATH=/mnt/atlswing/site-packages/ python -c "import jedi.client as jc; print jc.killTask(%s)"' % (task_id),
-                          "2>/dev/null"
-                         ],
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = p.communicate()
-    return out
+    return _execJediCommand(task_id, "killTask")
 
 def changeTaskPriority(task_id, priority):
-    p = subprocess.Popen(["ssh", "-i", rsa_key_file, "sbelov@aipanda015",
-                          'PYTHONPATH=/mnt/atlswing/site-packages/ python -c "import jedi.client as jc; print jc.changeTaskPriority(%s,%s)"' % (task_id, priority),
-                          "2>/dev/null"
-                         ],
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return _execJediCommand(task_id, "changeTaskPriority", priority)
 
-    out = p.communicate()
-    return out
+def reassignTaskToSite(task_id, site):
+    return _execJediCommand(task_id, "reassignTaskToSite", site)
+
+def reassignTaskToCloud(task_id, cloud):
+    return _execJediCommand(task_id, "reassignTaskToCloud", cloud)
+
--- atlas/prodtask/task_manage_views.py
+++ atlas/prodtask/task_manage_views.py
@@ -4,89 +4,87 @@ from django.shortcuts import render, render_to_response
 from django.template import Context, Template, RequestContext
 from django.template.loader import get_template
 from django.template.response import TemplateResponse
-
-from ..settings import defaultDatetimeFormat
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 
 import core.datatables as datatables
+from core.resource.models import Schedconfig
 
 from .forms import ProductionTaskForm, ProductionTaskCreateCloneForm, ProductionTaskUpdateForm
 from .models import ProductionTask, TRequest
 
 from .task_views import ProductionTaskTable
 
-from .task_commands import killTask, changeTaskPriority
+from .task_commands import killTask, changeTaskPriority, reassignTaskToSite, reassignTaskToCloud
 
-from django.db.models import Count, Q
-
-
-from django.utils.timezone import utc
-from datetime import datetime
-
+import ast
 import time
 import json
+import locale
+
+_task_actions = {
+    'kill': killTask,
+    'change_priority': changeTaskPriority,
+    'reassign_to_site': reassignTaskToSite,
+    'reassign_to_cloud': reassignTaskToCloud,
+}
+
+def do_tasks_action(tasks, action, *params):
+    if (not tasks) or not (action in _task_actions):
+        return
+
+    result = []
+    for task in tasks:
+        response = _task_actions[action](task, *params)
+        req_info = dict(task_id=task, action=action, response=response)
+        result.append(req_info)
+
+    return result
 
 
-def tasks_abort(request):
+def tasks_action(request, action):
     empty_response = HttpResponse('')
 
-    if request.method == 'POST':
-        data_json = request.POST.get('data')
-        if not data_json:
-            return empty_response
-        data = json.loads(data_json)
-        tasks = data.get('tasks')
-        if not tasks:
-            return empty_response
+    if request.method != 'POST' or not (action in _task_actions):
+        return empty_response
 
-        statuses = {}
-        for task in tasks:
-            res = killTask(task)
-            status = 'error'
-            if res and (res[0].startswith("(0,")):
-                status = 'ok'
-            statuses[task] = { 'status': status, 'reason': res[0]}
+    data_json = request.body
+    if not data_json:
+        return empty_response
+    data = json.loads(data_json)
 
-        response = { 'statuses': statuses }
-        return HttpResponse(json.dumps(response))
+    tasks = data.get("tasks")
+    if not tasks:
+        return empty_response
 
-    return empty_response
+    params = data.get("parameters", [])
+    response = do_tasks_action(tasks, action, *params)
+    return HttpResponse(json.dumps(response))
 
 
-def tasks_change_priority(request):
-    empty_response = HttpResponse('')
-
-    if request.method == 'POST':
-        data_json = request.POST.get('data')
-        if not data_json:
-            return empty_response
-        data = json.loads(data_json)
-        tasks = data.get('tasks')
-        priority = data.get('priority')
-        if (not tasks) or (priority is None):
-            return empty_response
-
-        statuses = {}
-        for task in tasks:
-            res = changeTaskPriority(task, priority)
-            status = 'ok' if (res and (res[0].startswith("(0,"))) else 'failed'
-            reason = str(res[0]) if len(res) > 1 else ''
-            statuses[task] = { 'status': status, 'reason': res[0]}
-
-        response = { 'statuses': statuses }
-        return HttpResponse(json.dumps(response))
-
-    return empty_response
-
-
-
+@ensure_csrf_cookie
+@csrf_protect
+@never_cache
 @datatables.datatable(ProductionTaskTable, name='fct')
 def task_manage(request):
     qs = request.fct.get_queryset()
     last_task_submit_time = ProductionTask.objects.order_by('-submit_time')[0].submit_time
-    return TemplateResponse(request, 'prodtask/_task_manage.html', { 'title': 'Manage Production Tasks',
-                                                                    'active_app' : 'prodtask',
-                                                                    'table': request.fct,
-                                                                    'parent_template': 'prodtask/_index.html',
-                                                                    'last_task_submit_time' : last_task_submit_time,
-                                                                    })
+
+    clouds = [ x.get('cloud') for x in Schedconfig.objects.using('default').values('cloud').distinct() ]
+    sites = [ x.get('siteid') for x in Schedconfig.objects.using('default').values('siteid').distinct() ]
+
+    locale.setlocale(locale.LC_ALL, '')
+    clouds = sorted(clouds, key=locale.strxfrm)
+    sites = sorted(sites, key=locale.strxfrm)
+
+    return TemplateResponse(request, 'prodtask/_task_manage.html',
+                            {'title': 'Manage Production Tasks',
+                             'active_app': 'prodtask/task_manage',
+                             'table': request.fct,
+                             'parent_template': 'prodtask/_index.html',
+                             'last_task_submit_time': last_task_submit_time,
+                             'clouds': clouds,
+                             'sites': sites,
+                             'edit_mode': True,
+                            })
 
