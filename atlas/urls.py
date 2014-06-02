626df9ff7d9c163325f24595b204260c371a33dc
jschovan <jschovan@gmail.com>
2014-06-02 05:30:24 +0000

    backporting updates from devel.

--- atlas/urls.py
+++ atlas/urls.py
@@ -15,14 +15,27 @@ common_patterns = patterns('',
     url('^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 
     ### the front page
-    url(r'^$', atlas_common_views.index, name='index'),
-    #url(r'^$', pandajob_views.listJobs, name='index'),
-
+#obsoleted.2014-05-01.jschovan#    url(r'^$', atlas_common_views.index, name='index'),
+    url(r'^$', include('core.pandajob.urls_pandajob_mainpage')),
 
     ### Applications
     url(r'^htcondorjobs', include('core.htcondor.urls')),
-    url(r'^job', include('core.pandajob.urls')),
-    url(r'^u', include('core.pandajob.urls_users', namespace='user')),
+#obsoleted.2014-05-01.jschovan#    url(r'^job', include('core.pandajob.urls')),
+    url(r'^jobs/', include('core.pandajob.urls_pandajob_jobs')),
+    url(r'^job/', include('core.pandajob.urls_pandajob_singlejob')),
+    url(r'^dash/', include('core.pandajob.urls_pandajob_dash')),
+    url(r'^support/', include('core.pandajob.urls_pandajob_support')),
+
+#obsoleted.2014-05-01.jschovan#    url(r'^u', include('core.pandajob.urls_users', namespace='user')),
+    url(r'^users/', include('core.pandajob.urls_pandajob_users')),
+    url(r'^user/', include('core.pandajob.urls_pandajob_singleuser')),
+
+    url(r'^tasks/', include('core.pandajob.urls_pandajob_tasks')),
+    url(r'^task/', include('core.pandajob.urls_pandajob_singletask')),
+
+    url(r'^sites/', include('core.pandajob.urls_pandajob_sites')),
+    url(r'^site/', include('core.pandajob.urls_pandajob_singlesite')),
+
     url(r'^resource', include('core.resource.urls')),
 ###     url(r'^api-auth', include('core.api.urls')),
 
@@ -31,9 +44,9 @@ common_patterns = patterns('',
     url(r'^api/datatables', include('core.table.urls')),
 #    url(r'^graphics/', include('core.graphics.urls')),
     url(r'^prodtask/', include('atlas.prodtask.urls', namespace='prodtask')),
+    url(r'^getdatasets/', include('atlas.getdatasets.urls', namespace='getdatasets')),
 
     url(r'^todo', include('atlas.todoview.urls', namespace='todoview')),
-
 
 #    ### TEST/Playground
 #    url(r'^test_playground/$', common_views.testing, name='testing'),
@@ -41,19 +54,11 @@ common_patterns = patterns('',
 #    url(r'^pan4/$', pandajob_views.list3PandaJobs, name='pan4'),
 #    url(r'^api-jediJobsInTask', include('core.api.jedi.jobsintask.urls')),
 #    url(r'^jediJobsInTask/', pandajob_views.jediJobsInTask, name='jediJobsInTask'),
-
 #    url(r'^api/jedi/', include('core.api.jedi.jobsintask.urls')),
 #    url(r'^api/user/', include('core.api.user.urls')),
 
-    url(r'^api-jedi', include('core.api.jedi.jobsintask.urls')),
-    url(r'^api-user', include('core.api.user.urls')),
-
-#    url(r'^userjobset/', pandajob_views.jediUserJobset, name='userjobset'),
-
-
-#    ### DEV
-#    url(r'^DEV-api-jediJobsInTask', include('core.api.jedi.jobsintask.urls')),
-#    url(r'^DEV-jediJobsInTask/', pandajob_views.DEVjediJobsInTask, name='DEV-jediJobsInTask'),
+#obsoleted.2014-05-01.jschovan#    url(r'^api-jedi', include('core.api.jedi.jobsintask.urls')),
+#obsoleted.2014-05-01.jschovan#    url(r'^api-user', include('core.api.user.urls')),
 
     ### Django Admin
     ### Uncomment the admin/doc line below to enable admin documentation:
