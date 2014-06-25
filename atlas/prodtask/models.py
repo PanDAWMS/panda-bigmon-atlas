13fea155ca216da60c4b765c6c63e54b917c4a07
mishaborodin <borodin.misha@gmail.com>
2014-06-25 10:15:38 +0000

    Increase comment field size

--- atlas/prodtask/models.py
+++ atlas/prodtask/models.py
@@ -168,7 +168,7 @@ class InputRequestList(models.Model):
     slice = models.DecimalField(decimal_places=0, max_digits=12, db_column='SLICE', null=False)
     brief = models.CharField(max_length=150, db_column='BRIEF')
     phys_comment = models.CharField(max_length=256, db_column='PHYSCOMMENT')
-    comment = models.CharField(max_length=256, db_column='SLICECOMMENT')
+    comment = models.CharField(max_length=512, db_column='SLICECOMMENT')
     input_data = models.CharField(max_length=150, db_column='INPUTDATA')
     project_mode = models.CharField(max_length=256, db_column='PROJECT_MODE')
     priority = models.DecimalField(decimal_places=0, max_digits=12, db_column='PRIORITY')
