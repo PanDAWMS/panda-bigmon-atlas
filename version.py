062d8f42b4179f7ecbee43bb1d4b00aa303f7c42
Jaroslava Schovancova <jschovan@gmail.com>
2014-08-04 22:30:54 -0400

    Migration from SVN to GitHub: adapting version string calculation.

--- version.py
+++ version.py
@@ -18,35 +18,47 @@ def get_version_base__release_type__provides():
     return (version_base, release_type, provides,)
 
 
-def get_svn_version():
+def get_git_version():
     """
-        get version of this svn commit
+        get version of this git commit
     """
     dir = os.path.dirname(os.path.realpath(__file__))
+    ### count cap of total number of commits in this repository
+    ncommits = 1
+    try:
+        ncommits = commands.getoutput('git shortlog | wc -l')
+    except:
+        pass
     ### get last revision ID, short version
     last_rev_id = ''
     try:
-        last_rev_id = commands.getoutput(' svnversion atlas/ ')
+        last_rev_id = commands.getoutput('git show -s --pretty=format:%h ')
     except:
         pass
-    return str('.dev-r' + last_rev_id)
+    ### get number of revisions on this branch
+    nrevs = 1
+    try:
+        nrevs = commands.getoutput('git reflog | wc -l')
+    except:
+        pass
+
+    return str('.dev-' + ncommits + '-' + last_rev_id + '-' + nrevs)
 
 
 def get_version_provides():
-#    __version__ = get_svn_version()
-#    __provides__ = get_version_base__release_type__provides()
-#    return (__version__, __provides__,)
     isStable = False
     version_base, release_type, provides = get_version_base__release_type__provides()
     if release_type == 'stable':
         isStable = True
     __version__ = version_base
     if not isStable:
-        __version__ += get_svn_version()
+        __version__ += get_git_version()
     __provides__ = provides
     return (__version__, __provides__,)
 
 
+
 __version__, __provides__ = get_version_provides()
+
 
 
