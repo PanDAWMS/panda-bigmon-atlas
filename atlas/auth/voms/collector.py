e25614652478431877137b779ec481df7bde916e
Sergey Belov <belov.sd@gmail.com>
2014-09-03 08:30:51 +0200

    Added handling for possible wrong data from VOMS

--- atlas/auth/voms/collector.py
+++ atlas/auth/voms/collector.py
@@ -1,11 +1,18 @@
+#!/bin/env python
+
+import re
 from django.conf import settings
 
 from .interface import VomsInterface
-
 from .models import VomsUser
 
 def run():
-    options = VomsInterface.get_identity_options()
+    """
+    Update VOMS mapping nickname <-> (DN, CA) in the database
+    :return: dictionary, 'added', 'removed' - records added/removed,
+             'details' - more information on operations performed
+    """
+    options = VomsInterface.get_identity_options(proxy_cert=settings.VOMS_PROXY_CERT)
     options.update(settings.VOMS_OPTIONS)
 
     voms = VomsInterface(options)
@@ -15,16 +22,35 @@ def run():
     for user_info in voms.list_users():
         dn = user_info["DN"]
         ca = user_info["CA"]
-        nickname = voms.get_user_nickname(dn, ca)
-
+        try:
+            nickname = voms.get_user_nickname(dn, ca)
+        except:
+            # TODO: log the error (e.g. user was removed during data collection)
+            continue
+        if not re.match(r"^\w+$", nickname):
+            # TODO: log the warning
+            continue
         if not voms_users.get(nickname):
             voms_users[nickname] = {}
         voms_users[nickname].update({dn: ca})
 
+    result = {'added': 0, 'removed': 0, 'detailed': [],}
+
     for user in VomsUser.objects.all():
         info = voms_users.get(user.username)
-        if not info or not (dn in voms_users[user.username]):
-            user.delete()
+        if not info or not dn in voms_users[user.username]:
+            try:
+                user.delete()
+            except:
+                # TODO: log the error
+                continue
+
+            result['removed'] += 1
+            result['detailed'].append({
+                'action': 'remove', 'username': user.username,
+                'dn': user.dn, 'ca': user.ca,
+            })
+            # TODO: log operation
             continue
         else:
             del voms_users[user.username][user.dn]
@@ -39,9 +65,19 @@ def run():
                 user.username = nickname
                 user.dn = dn
                 user.ca = ca
-                user.save()
+                try:
+                    user.save()
+                except:
+                    # TODO: log the error
+                    continue
+                result['added'] += 1
+                result['detailed'].append({
+                    'action': 'add', 'username': user.username,
+                    'dn': user.dn, 'ca': user.ca,
+                })
+
+    return result
 
 
 if __name__ == "__main__":
     run()
-
