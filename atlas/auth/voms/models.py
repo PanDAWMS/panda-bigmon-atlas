c37671b27a1d8ae2b185ed56bb38a00bbb35d8b5
Sergey Belov <belov.sd@gmail.com>
2014-09-03 08:29:48 +0200

    Model changed to work both with Oracle and Sqlite

--- atlas/auth/voms/models.py
+++ atlas/auth/voms/models.py
@@ -4,10 +4,11 @@ from django.db import models
 from django.utils import timezone
 
 class VomsUser(models.Model):
-    username = models.CharField(max_length=60, db_column='username', primary_key=True)
-    dn = models.CharField(max_length=255, db_column='dn')
-    ca = models.CharField(max_length=255, db_column='ca')
-    added_on = models.DateTimeField(auto_now_add=True, db_column='added_on')
+    id = models.AutoField(db_column='ID', primary_key=True)
+    username = models.CharField(max_length=60, db_column='USERNAME', db_index=True)
+    dn = models.CharField(max_length=255, db_column='DN', db_index=True)
+    ca = models.CharField(max_length=255, db_column='CA')
+    added_on = models.DateTimeField(auto_now_add=True, db_column='ADDED_ON')
 
     def save(self, *args, **kwargs):
         if not self.added_on:
@@ -17,5 +18,6 @@ class VomsUser(models.Model):
 
     class Meta:
         managed = True
-        db_table = u'"ATLAS_DEFT"."VOMS_USERS_MAP"'
+        db_table = u'VOMS_USERS_MAP'
+        app_label = 'auth'
         unique_together = (("username", "dn"),)
