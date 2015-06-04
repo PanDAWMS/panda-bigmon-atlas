0735b1d7192b69ef3543c5bca6dcda6693922591
ruslan33 <rmashinistov@gmail.com>
2015-06-04 12:16:16 +0200

    Correct wrong commit

--- atlas/manage.py
+++ atlas/manage.py
@@ -10,8 +10,7 @@ DJANGO_SETTINGS_MODULE = '%s.%s' % (split(abspath(dirname(__file__)))[1], 'setti
 # pythonpath dirs
 PYTHONPATH = [
     join(dirname(__file__), pardir),
-    "/data/monitor"
-    #"..//..//bigpandamon-core",
+    "..//..//bigpandamon-core",
 ]
 
 # inject few paths to pythonpath
