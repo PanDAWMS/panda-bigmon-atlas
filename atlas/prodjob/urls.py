241e066dbb49d332c54f9108c49acc5bac7a23a6
ruslan33 <rmashinistov@gmail.com>
2015-10-20 17:29:27 +0300

    Preparation for job management engine

--- atlas/prodjob/urls.py
+++ atlas/prodjob/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'atlas.prodjob.views.request_jobs',       name='request_jobs'),
+    url(r'^jobs_action/$', 'atlas.prodjob.views.jobs_action', name='jobs_action'),
 )
 
 
--- atlas/prodjob/views.py
+++ atlas/prodjob/views.py
@@ -2,17 +2,22 @@
 # import logging
 # import os
 
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 #_logger = logging.getLogger('prodtaskwebui')
 
-jlist = [{"id":1},{"id":2}]
+
 
 
 
 def request_jobs(request):
+    jlist = [{"id":1},{"id":2}]
     if request.method == 'POST':
 		return render(request, '_job_table.html', {'jlist':jlist})
     else:
         return render(request, '_job_table.html', {'jlist':jlist})
+
+def jobs_action(request):
+    print(request.body)
+    return render(request, '_job_table.html', {'jlist':[{"id":1},{"id":4}]})
