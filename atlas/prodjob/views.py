8789d5ef315cc0269ab31c6c4e92be2f1cf1cba7
ruslan33 <rmashinistov@gmail.com>
2015-10-22 16:11:13 +0300

    Preparation for job management engine

--- atlas/prodjob/views.py
+++ atlas/prodjob/views.py
@@ -6,36 +6,31 @@ import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 
-#_logger = logging.getLogger('prodtaskwebui')
+
+# _logger = logging.getLogger('prodtaskwebui')
 
 
 
 
 
 def request_jobs(request):
-    jlist = [{"id":1},{"id":2}]
-    if request.method == 'POST':
-		return render(request, '_job_table.html', {'jlist':jlist})
-    else:
-        return render(request, '_job_table.html', {'jlist':jlist})
+    return render(request, '_job_table.html')
+
 
 def jobs_action(request):
-#    print(request.body);
-#    curl -H 'Accept: application/json' -H 'Content-Type: application/json' "http://bigpanda.cern.ch/jobs/?pandaid=2646731860,2646731861";
-    url = 'http://bigpanda.cern.ch/jobs/?pandaid=2646731860,2646731861';
-    headers = {'content-type': 'application/json','accept': 'application/json'};
-    resp = requests.get(url, headers=headers)
-    data = json.loads(resp.text);
-    print data;
-    jlist = [];
-    for job in data:
-        jlist.append([job['pandaid'],job['jobstatus']]);
+    print request.body;
+    return HttpResponse('OK')
 
-
-
-    return HttpResponse(json.dumps(jlist))
 
 def get_jobs(request):
-    print("TEST")
-    jlist = [{"id":3},{"id":4}]
+    #    print(request.body);
+    #    curl -H 'Accept: application/json' -H 'Content-Type: application/json' "http://bigpanda.cern.ch/jobs/?pandaid=2646731860,2646731861";
+    url = 'http://bigpanda.cern.ch/jobs/?pandaid=2646731860,2646731861';
+    headers = {'content-type': 'application/json', 'accept': 'application/json'};
+    resp = requests.get(url, headers=headers)
+    data = json.loads(resp.text);
+    #print data;
+    jlist = [];
+    for job in data:
+        jlist.append([job['pandaid'], job['jobstatus']])
     return HttpResponse(json.dumps(jlist))
