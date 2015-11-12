36f4bdfb7a902f12a850da03610ed84407fdda00
ruslan33 <rmashinistov@gmail.com>
2015-11-12 15:11:57 +0100

    Job manage interface

--- atlas/settings/base.py
+++ atlas/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS_BIGPANDAMON_ATLAS = (
     'core.datatables',
 #    'core.gspread', #NOT-NEEDED-IN-ATLAS?
     'atlas.prodtask',
+    'atlas.prodjob',
     ### atlas.todoview: Placeholder for views which need to be implemented
     ### as part of cross-linking between jobs and tasks monitoring
     'atlas.todoview',
