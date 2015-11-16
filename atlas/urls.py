032d804a2a10e301e8c9d35e736fc5f3f418ae69
ruslan33 <rmashinistov@gmail.com>
2015-11-16 09:27:33 +0100

    Add prodjobs to urls

--- atlas/urls.py
+++ atlas/urls.py
@@ -47,6 +47,7 @@ common_patterns = patterns('',
     url(r'^api/datatables', include('core.table.urls')),
 #    url(r'^graphics/', include('core.graphics.urls')),
     url(r'^prodtask/', include('atlas.prodtask.urls', namespace='prodtask')),
+    url(r'^prodjob/', include('atlas.prodjob.urls', namespace='prodjob')),
     url(r'^getdatasets/', include('atlas.getdatasets.urls', namespace='getdatasets')),
 
     url(r'^todo', include('atlas.todoview.urls', namespace='todoview')),
