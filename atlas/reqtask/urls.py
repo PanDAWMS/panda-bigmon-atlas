240e4b872beae8c7e0f228f85ffb2c7fa8fe433d
ruslan33 <rmashinistov@gmail.com>
2015-11-30 16:26:34 +0100

    Task for Request interface

---
+++ atlas/reqtask/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'atlas.reqtask.views.request_tasks',       name='request_tasks'),
+    url(r'^tasks_action/$', 'atlas.reqtask.views.tasks_action', name='tasks_action'),
+    url(r'^get_tasks/$', 'atlas.reqtask.views.get_tasks', name='get_tasks'),
+)
+
+
---
+++ atlas/reqtask/views.py
@@ -0,0 +1,71 @@
+import json
+import requests
+from django.core import serializers
+from atlas.prodtask.models import ProductionTask
+# import logging
+# import os
+import simplejson
+from decimal import Decimal
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# _logger = logging.getLogger('prodtaskwebui')
+
+
+def request_tasks(request):
+    #get_tasks(request);
+    return render(request, 'reqtask/_task_table.html')
+
+
+def tasks_action(request):
+    """
+
+    :type request: object
+    """
+    user = request.user.username
+
+    is_superuser = request.user.is_superuser
+    #print request.body
+    if not is_superuser:
+        return HttpResponse('Permission denied')
+
+    return HttpResponse('OK')
+
+
+def get_task_array():
+    task_array = [7090637,7090622,7090621,7090620,7090619]
+    return task_array
+
+
+def get_tasks(request):
+
+
+    #low_reqid = json.loads(request.body)["low_reqid"]
+    #high_reqid = json.loads(request.body)["high_reqid"]
+    #reqid = json.loads(request.body)[0];
+    #qs = ProductionTask.objects.filter(request__reqid=reqid).values()
+    task_array = get_task_array()
+
+    qs = ProductionTask.objects.filter(id__in=task_array).values()
+    #qs = ProductionTask.objects.filter(request__reqid__range=(low_reqid,high_reqid)).values()
+    #data = serializers.serialize('json', qs)
+
+    def decimal_default(obj):
+        if isinstance(obj, Decimal):
+
+            return float(obj)
+        if isinstance(obj, datetime):
+
+            return obj.isoformat()
+
+        raise TypeError
+
+    data = json.dumps(list(qs),default = decimal_default)
+
+
+    #
+    #
+    return HttpResponse(data)
