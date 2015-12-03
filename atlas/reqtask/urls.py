658273c86215c86d17efd3ddd01793a6ad2d9b44
ruslan33 <rmashinistov@gmail.com>
2015-12-03 12:28:31 +0100

    Correct url

--- atlas/reqtask/urls.py
+++ atlas/reqtask/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    #url(r'^$', 'atlas.reqtask.views.request_tasks',       name='request_tasks'),
-    url(r'^(?P<rid>\d+)/$|^$', 'atlas.reqtask.views.request_tasks',       name='request_tasks'),
+    url(r'^$', 'atlas.reqtask.views.request_tasks',       name='request_tasks'),
+    url(r'^(?P<rid>\d+)/$', 'atlas.reqtask.views.request_tasks',       name='request_tasks_rid'),
+    #url(r'^(?P<rid>\d+)/$|^$', 'atlas.reqtask.views.request_tasks',       name='request_tasks'),
     url(r'^tasks_action/$', 'atlas.reqtask.views.tasks_action', name='tasks_action'),
     url(r'^get_tasks/$', 'atlas.reqtask.views.get_tasks', name='get_tasks'),
 )
