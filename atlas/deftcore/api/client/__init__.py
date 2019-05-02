996c6f259af108c35474d4f6cb1def37c0bd33fa
mishaborodin <borodin.misha@gmail.com>
2019-05-02 17:02:55 +0200

    update deft proxy

--- atlas/deftcore/api/client/__init__.py
+++ atlas/deftcore/api/client/__init__.py
@@ -1,5 +1,4 @@
 __author__ = 'Dmitry Golubkov'
-__email__ = 'dmitry.v.golubkov@cern.ch'
 
 import json
 import requests
@@ -7,35 +6,30 @@ import urllib
 
 
 class Client(object):
-    BASE_URL = 'https://aipanda015.cern.ch'
-
-    def __init__(self, auth_user, auth_key, verify_ssl_cert=False, base_url=None):
+    def __init__(self, auth_user, auth_key, verify_ssl_cert=False, base_url='https://aipanda015.cern.ch'):
+        self.base_url = base_url
         self.verify_ssl_cert = verify_ssl_cert
-        if base_url:
-            self.base_url = base_url
-        else:
-            self.base_url = self.BASE_URL
         self.api_url = '/api/v1/request/'
         self.api_search_task_url = '/api/v1/task/'
         self.headers = {'Content-Type': 'application/json',
-                        'Authorization': "ApiKey %s:%s" % (auth_user, auth_key)}
+                        'Authorization': 'ApiKey {0}:{1}'.format(auth_user, auth_key)}
 
     def _get_action_list(self):
-        url = "%s%sactions/" % (self.base_url, self.api_url)
+        url = '{0}{1}actions/'.format(self.base_url, self.api_url)
         response = requests.get(url, headers=self.headers, verify=self.verify_ssl_cert)
         if response.status_code == requests.codes.ok:
             return json.loads(response.content)['result']
         else:
-            raise Exception("Invalid HTTP response code: %d" % response.status_code)
+            raise Exception('Invalid HTTP response code: {0}'.format(response.status_code))
 
     def _create_request(self, action, owner, body):
         action_list = self._get_action_list()
         if not action in action_list:
-            raise Exception("Invalid action: %s (%s)" % (action, str(action_list)))
+            raise Exception('Invalid action: {0} ({1})'.format(action, str(action_list)))
 
         url = "%s%s" % (self.base_url, self.api_url)
 
-        data = {'action': action, 'owner': owner, 'body': "%s" % json.dumps(body)}
+        data = {'action': action, 'owner': owner, 'body': json.dumps(body)}
 
         response = requests.post(url, headers=self.headers, data=json.dumps(data), verify=self.verify_ssl_cert)
 
@@ -43,21 +37,21 @@ class Client(object):
             api_request_object = json.loads(response.content)
             return api_request_object['id']
         elif response.status_code == requests.codes.unauthorized:
-            raise Exception("Access denied")
+            raise Exception('Access denied')
         else:
-            raise Exception("Invalid HTTP response code: %d" % response.status_code)
+            raise Exception('Invalid HTTP response code: {0}'.format(response.status_code))
 
     def get_status(self, request_id):
-        url = "%s%s%s/" % (self.base_url, self.api_url, request_id)
+        url = '{0}{1}{2}/'.format(self.base_url, self.api_url, request_id)
         response = requests.get(url, headers=self.headers, verify=self.verify_ssl_cert)
         if response.status_code == requests.codes.ok:
             status_string = json.loads(response.content)['status']
             if status_string:
                 return json.loads(status_string)
         elif response.status_code == requests.codes.unauthorized:
-            raise Exception("Access denied")
+            raise Exception('Access denied')
         else:
-            raise Exception("Invalid HTTP response code: %d" % response.status_code)
+            raise Exception('Invalid HTTP response code: {0}'.format(response.status_code))
 
     def create_task_chain(self, owner, step_id, max_number_of_steps=None, debug_mode=False):
         body = {'step_id': step_id, 'max_number_of_steps': max_number_of_steps, 'debug_mode': debug_mode}
@@ -107,8 +101,8 @@ class Client(object):
         body = {'task_id': task_id}
         return self._create_request('resume_task', owner, body)
 
-    def reassign_task_to_share(self, owner, task_id, share):
-        body = {'task_id': task_id, 'share': share}
+    def reassign_task_to_share(self, owner, task_id, share, reassign_running=False):
+        body = {'task_id': task_id, 'share': share, 'reassign_running': reassign_running}
         return self._create_request('reassign_task_to_share', owner, body)
 
     def trigger_task_brokerage(self, owner, task_id):
@@ -175,12 +169,12 @@ class Client(object):
         if len(filter_dict.keys()):
             filter_dict.update({'limit': 0})
         filter_string = urllib.urlencode(filter_dict)
-        url = "%s%s?%s" % (self.base_url, self.api_search_task_url, filter_string)
+        url = '{0}{1}?{2}'.format(self.base_url, self.api_search_task_url, filter_string)
         response = requests.get(url, headers=self.headers, verify=self.verify_ssl_cert)
         if response.status_code == requests.codes.ok:
             return json.loads(response.content)
         else:
-            raise Exception("Invalid HTTP response code: %d" % response.status_code)
+            raise Exception('Invalid HTTP response code: {0}'.format(response.status_code))
 
     def search_task_by_id(self, task_id):
         filter_dict = dict()
@@ -207,14 +201,6 @@ class Client(object):
         return self._search_task(filter_dict)
 
     def create_slice_tier0(self, owner, slice_dict, steps_list):
-        """
-        Create slice in last tier0 request
-        :param slice_dict: dict, possible keys: ['dataset', 'comment', 'priority']
-        :param steps_list: list of dicts, possible keys ['ctag', 'output_formats', 'memory', 'priority'] +
-        ['input_format', 'nEventsPerJob', 'token', 'merging_tag', 'nFilesPerMergeJob', 'nGBPerMergeJob'] +
-        ['nMaxFilesPerMergeJob', 'project_mode', 'nFilesPerJob', 'nGBPerJob', 'maxAttempt']
-        :return: API request ID if succeed
-        """
         body = {'slice_dict': slice_dict, 'steps_list': steps_list}
         return self._create_request('create_slice_tier0', owner, body)
 
