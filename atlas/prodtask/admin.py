416483bb4c26456297d58d4d484f2edca1f21628
mishaborodin <borodin.misha@gmail.com>
2019-12-03 17:00:06 +0100

    Update admin web

--- atlas/prodtask/admin.py
+++ atlas/prodtask/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, User, Group
 admin.site.register(Permission)
-
+admin.site.register(User)
+admin.site.register(Group)
