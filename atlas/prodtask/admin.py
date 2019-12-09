4607f84564cea72d0bce0c3dc98e2483239c7010
mishaborodin <borodin.misha@gmail.com>
2019-12-09 17:22:46 +0100

    Add user admin

--- atlas/prodtask/admin.py
+++ atlas/prodtask/admin.py
@@ -2,6 +2,11 @@
 from django.contrib import admin
 
 from django.contrib.auth.models import Permission, User, Group
+
+
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Permission)
-admin.site.register(User)
-admin.site.register(Group)
