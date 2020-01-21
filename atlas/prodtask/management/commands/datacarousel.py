b938b6e8d28dbbbe7d4e37c1566f42ffee234732
mishaborodin <borodin.misha@gmail.com>
2020-01-21 15:29:56 +0100

    Fix permission for groups

---
+++ atlas/prodtask/management/commands/datacarousel.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+import time
+
+from atlas.prestage.views import submit_all_tapes
+
+
+class Command(BaseCommand):
+    args = 'None'
+    help = 'Submit request for all tapes '
+
+    def handle(self, *args, **options):
+        self.stdout.write('Start submit tape rules %s'%time.ctime())
+        if not args:
+            try:
+                submit_all_tapes()
+            except Exception as e:
+                raise CommandError('Some problem during tape rules check: %s'%e)
+        self.stdout.write('Successfully finished tape rules  check: %s'%time.ctime())
