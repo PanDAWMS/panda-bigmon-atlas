38594e37c3ff8a7f1a36a798987b3bd9626f6218
mishaborodin <borodin.misha@gmail.com>
2020-01-23 16:56:02 +0100

    fix syncing jedideft

--- atlas/prodtask/management/commands/syncjedideft.py
+++ atlas/prodtask/management/commands/syncjedideft.py
@@ -8,12 +8,16 @@ class Command(BaseCommand):
     args = '<task_id>'
     help = 'Sync tasks < task_id'
 
+    def add_arguments(self, parser):
+        parser.add_argument('start_id', nargs=1, type=int)
+
     def handle(self, *args, **options):
         self.stdout.write('Start sync from request to tasks at %s'%time.ctime())
-        if args:
+        if options:
             try:
-                task_id = int(args[0])
+                task_id = int(options['start_id'][0])
                 sync_old_tasks(task_id)
+                pass
             except Exception as e:
                 raise CommandError('Some problem during syncing: %s'%str(e))
         self.stdout.write('Successfully finished tasks sync: %s'%time.ctime())
