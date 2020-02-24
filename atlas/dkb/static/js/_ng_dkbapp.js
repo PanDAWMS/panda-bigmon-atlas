4beb72ccdbc4307fd08549c71dd1ae72fa8efbea
mishaborodin <borodin.misha@gmail.com>
2020-02-24 11:46:31 +0100

    Change PB to 10^12 instead of 2^40

--- atlas/dkb/static/js/_ng_dkbapp.js
+++ atlas/dkb/static/js/_ng_dkbapp.js
@@ -244,30 +244,30 @@
                     if (isNaN(size))
                         size = 0;
 
-                    if (size < 1024)
+                    if (size < 1000)
                         return size + ' B';
 
-                    size /= 1024;
+                    size /= 1000;
 
-                    if (size < 1024)
+                    if (size < 1000)
                         return size.toFixed(2) + ' KB';
 
-                    size /= 1024;
+                    size /= 1000;
 
-                    if (size < 1024)
+                    if (size < 1000)
                         return size.toFixed(2) + ' MB';
 
-                    size /= 1024;
+                    size /= 1000;
 
-                    if (size < 1024)
+                    if (size < 1000)
                         return size.toFixed(2) + ' GB';
 
-                    size /= 1024;
+                    size /= 1000;
 
-                    if (size < 1024)
+                    if (size < 1000)
                         return size.toFixed(2) + ' TB';
 
-                    size /= 1024;
+                    size /= 1000;
 
                     return size.toFixed(2) + ' PB';
 
