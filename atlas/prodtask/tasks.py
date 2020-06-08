e11a195c18b214f507bd7320d3653e493010fc1c
mishaborodin <borodin.misha@gmail.com>
2020-06-08 18:16:12 +0200

    add celery tasks to replace cron

--- atlas/prodtask/tasks.py
+++ atlas/prodtask/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from atlas.celerybackend.celery import app
+from atlas.prestage.views import find_action_to_execute, submit_all_tapes_processed
 from atlas.prodtask.task_views import sync_old_tasks
 
 import logging
@@ -15,4 +16,15 @@ def test_celery():
 @app.task(ignore_result=True)
 def sync_tasks():
     sync_old_tasks(-1)
-    return 2
+    return None
+
+
+@app.task(ignore_result=True)
+def step_actions():
+    find_action_to_execute()
+    return None
+
+@app.task(ignore_result=True)
+def data_carousel():
+    submit_all_tapes_processed()
+    return None
