9b40f5a8047504ce6f8d482aead645384e79f938
mishaborodin <borodin.misha@gmail.com>
2021-02-18 16:50:01 +0100

    New ts for requests

---
+++ atlas/frontendjs/prodsysjs/src/app/production-request/slice/slice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slice',
+  templateUrl: './slice.component.html',
+  styleUrls: ['./slice.component.css']
+})
+export class SliceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
