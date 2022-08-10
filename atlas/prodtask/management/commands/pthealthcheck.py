e6303e7c43c0ac9727e7c110cac4d1b6b9b9784f
mishaborodin <borodin.misha@gmail.com>
2022-08-10 22:43:43 +0200

    Add celery beat health check

---
+++ atlas/prodtask/management/commands/pthealthcheck.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand, CommandError
+import time
+
+from django_celery_beat.models import PeriodicTask
+from django.utils import timezone
+from datetime import timedelta
+
+from atlas.prodtask.views import send_alarm_message
+
+
+class Command(BaseCommand):
+    args = 'None'
+    help = 'Check celery beat health'
+
+    def handle(self, *args, **options):
+        if not args:
+            try:
+                try:
+                    last_executed_task = PeriodicTask.objects.all().order_by('-last_run_at')[0]
+                except Exception as e:
+                    send_alarm_message('Alarm: the celery beat health check problem',
+                              f'Celery beat health check problem {e}')
+                    raise e
+                if (timezone.now() - last_executed_task.last_run_at) < timedelta(hours=2):
+                    send_alarm_message('Alarm: the celery beat is stuck',
+                                       f'Celery beat last updated {last_executed_task.last_run_at}')
+            except Exception as e:
+                raise CommandError('Some problem during alarm mail sending check: %s'%e)
