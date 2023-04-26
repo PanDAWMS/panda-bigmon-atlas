2a886cb918311150623550c8f88a115d70e45d4b
mishaborodin <borodin.misha@gmail.com>
2023-04-26 13:51:49 +0200

    Fix AMI server

--- atlas/settings/amiclient.py
+++ atlas/settings/amiclient.py
@@ -1,3 +1,4 @@
-AMI_API_V2_BASE_URL = 'https://ami.in2p3.fr/AMI2/api/'
-
-CERTIFICAT = "/tmp/x509_prodsys_mon"
+#AMI_API_V2_BASE_URL = 'https://ami.in2p3.fr/AMI2/api/'
+AMI_API_V2_BASE_URL = 'https://atlas-ami.cern.ch/AMI2/api/'
+from .local import CERTIFICATE_PATH
+CERTIFICAT = CERTIFICATE_PATH
