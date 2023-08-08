77061319ca87d94b5b56915b6de095af49a260cb
mishaborodin <borodin.misha@gmail.com>
2023-08-09 00:18:11 +0200

    new sso preparation

--- atlas/auth/oidcsso/backends.py
+++ atlas/auth/oidcsso/backends.py
@@ -7,9 +7,12 @@ class OIDCCernSSOBackend(RemoteUserBackend):
     create_unknown_user = False
 
     def authenticate(self, request, remote_user):
-        user_info = json.loads(request.META.get('OIDC_userinfo_json'))
-        user_groups = json.loads(request.META.get('OIDC_CLAIM_resource_access')).get('atlas-prodtask').get('roles')
-        _logger.info(f'authenticate {user_info}')
-        _logger.info(f'authenticate {user_groups}')
+        try:
+            user_info = json.loads(request.META.get('OIDC_userinfo_json'))
+            user_groups = json.loads(request.META.get('OIDC_CLAIM_resource_access')).get('atlas-prodtask').get('roles')
+            _logger.info(f'authenticate {user_info}')
+            _logger.info(f'authenticate {user_groups}')
+        except:
+            pass
 
         return super().authenticate(request, remote_user)
