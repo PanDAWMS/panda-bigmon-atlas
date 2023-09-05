9ff15656c9c2e8b9a29e528bd0e1c2d11ba856d1
mishaborodin <borodin.misha@gmail.com>
2023-09-05 12:49:54 +0200

    CA CERT for AMI client

--- atlas/settings/amiclient.py
+++ atlas/settings/amiclient.py
@@ -1,4 +1,5 @@
 #AMI_API_V2_BASE_URL = 'https://ami.in2p3.fr/AMI2/api/'
 AMI_API_V2_BASE_URL = 'https://atlas-ami.cern.ch/AMI2/api/'
-from .local import CERTIFICATE_PATH
+from .local import CERTIFICATE_PATH, ATLAS_ES
 CERTIFICAT = CERTIFICATE_PATH
+CA_CERTIFICATES = ATLAS_ES['ca_cert']
