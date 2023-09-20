ab2508b06d7985cbf1b9439eb9b534d0f8bfbf50
mishaborodin <borodin.misha@gmail.com>
2023-09-20 17:46:02 +0200

    Add auth to everything

--- atlas/settings/base.py
+++ atlas/settings/base.py
@@ -23,9 +23,9 @@ MIDDLEWARE = (
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
      AUTH_MIDDL,
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
