e5073c44f746eb49bffbc53ca1fd8bd21e9b2416
mishaborodin <borodin.misha@gmail.com>
2024-05-06 21:04:41 +0200

    Fix dkb to scale

--- atlas/dkb/views.py
+++ atlas/dkb/views.py
@@ -410,26 +410,29 @@ def form_statistic_per_step(statistics,running_stat, finished_stat, mc_steps=Tru
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes((IsAuthenticated,))
 def output_hashtag_stat(request):
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
     try:
         hashtags_raw = request.data['hashtag']
         task_ids = tasks_from_string(hashtags_raw)
         steps={}
-        status_stat = [{'name': 'total', 'count': None}]
-        if len(task_ids)<10000:
-            tasks = list(ProductionTask.objects.filter(id__in=task_ids).values('status','ami_tag','output_formats'))
-            for task in tasks:
-                step_name = task['output_formats']+' '+task['ami_tag']
-                if step_name not in steps:
-                    steps[step_name] = {}
-                if task['status'] not in steps[step_name]:
-                    steps[step_name][task['status']] = 0
-                steps[step_name][task['status']] += 1
-            status_dict = dict([(x['status'], x['count']) for x in
-                  ProductionTask.objects.filter(id__in=task_ids).values('status').annotate(count=Count('id'))])
-            status_stat = [{'name':'total','count':sum(status_dict.values())}]
-            for status in ProductionTask.STATUS_ORDER:
-                if status in status_dict:
-                    status_stat.append({'name': status, 'count': status_dict[status]})
+        status_dict = {}
+        tasks = sum([list(ProductionTask.objects.filter(id__in=chunk).values('status','ami_tag','output_formats')) for chunk in chunks(task_ids, 1000)], [])
+        for task in tasks:
+            step_name = task['output_formats']+' '+task['ami_tag']
+            if step_name not in steps:
+                steps[step_name] = {}
+            if task['status'] not in steps[step_name]:
+                steps[step_name][task['status']] = 0
+            steps[step_name][task['status']] += 1
+            status_dict[task['status']] = status_dict.get(task['status'],0)+1
+        status_stat = [{'name':'total','count':sum(status_dict.values())}]
+        for status in ProductionTask.STATUS_ORDER:
+            if status in status_dict:
+                status_stat.append({'name': status, 'count': status_dict[status]})
         hashtags_split = hashtags_raw.replace('&',',').replace('|',',').split(',')
         hashtags = [x.lower() for x in hashtags_split if x]
         #format_dict = deriv_formats({"terms": {"hashtag_list": hashtags}})
