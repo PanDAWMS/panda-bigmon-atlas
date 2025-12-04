1fe83fd292a55ac8c36cd647af5674c438cb15c6
mishaborodin <borodin.misha@gmail.com>
2025-12-04 12:36:16 +0100

    hepmc new format

--- atlas/deftcore/protocol/__init__.py
+++ atlas/deftcore/protocol/__init__.py
@@ -229,7 +229,7 @@ class Protocol(object):
             "param_type": "output",
             "token": "ATLASDATADISK",
             "type": "template",
-            "value": "{{name}}={{data_type}}.{{task_id|stringformat:\".08d\"}}._${SN}.tar.gz"
+            "value": "{{name}}={{data_type}}.{{task_id|stringformat:\".08d\"}}._${SN}.gz"
         }""",
         # FIXME: OverlayTest
         TaskParamName.TXT_EVENTID_OUTPUT: """{
