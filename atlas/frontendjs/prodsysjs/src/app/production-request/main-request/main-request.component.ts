ead07cbf1fcaed602f004835d9631e2a1771fd11
mishaborodin <borodin.misha@gmail.com>
2026-03-24 12:58:56 +0100

    Prepare for migration

--- atlas/frontendjs/prodsysjs/src/app/production-request/main-request/main-request.component.ts
+++ atlas/frontendjs/prodsysjs/src/app/production-request/main-request/main-request.component.ts
@@ -1,13 +1,11 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit, signal, Signal, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, inject, OnInit, signal, ViewChild} from '@angular/core';
 import {PatternSelectionComponent} from '../pattern-selection/pattern-selection.component';
-import {CampaignPattern} from '../production-request-models';
-import {CdkVirtualForOf, CdkVirtualScrollViewport, ScrollingModule} from "@angular/cdk/scrolling";
-import {DataSource} from "@angular/cdk/collections";
-import {MatTableDataSource} from "@angular/material/table";
-import {FormsModule} from "@angular/forms";
-import {toSignal} from "@angular/core/rxjs-interop";
-
-
+import {CampaignPattern, ProductionRequests, ProductionTask, Slice} from '../production-request-models';
+import {CdkVirtualScrollViewport, ScrollingModule} from '@angular/cdk/scrolling';
+import {MatTableDataSource} from '@angular/material/table';
+import {FormsModule} from '@angular/forms';
+import {ProductionRequestService} from '../production-request.service';
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -25,7 +23,8 @@ export class MainRequestComponent implements OnInit, AfterViewInit {
 
   @ViewChild('scrollViewport') scrollViewport: CdkVirtualScrollViewport;
 
-
+  productionRequestService = inject(ProductionRequestService);
+  router = inject(Router);
   mockPatterns: CampaignPattern[] = [
     {
       campaign: 'MC23',
@@ -75,84 +74,114 @@ export class MainRequestComponent implements OnInit, AfterViewInit {
       ]
     }
   ];
-  rows: ColumnsWihtCheckbox[] = this.createMockRows();
-  rowsDatasource: MatTableDataSource<ColumnsWihtCheckbox>;
+  rowsDatasource: MatTableDataSource<ProdReqColumnsWithCheckbox>;
+  indexMap: Map<number, number> = new Map<number, number>();
+  renderedSlicesMap: Map<number, ProdReqColumnsWithCheckbox[]> = new Map<number, ProdReqColumnsWithCheckbox[]>();
   scrollIndex = 0;
   filterString = '';
   currentIndex = signal(0);
+  productionRequests: ProductionRequests;
   ngOnInit() {
-    this.rowsDatasource = new MatTableDataSource<ColumnsWihtCheckbox>(this.rows);
+    this.rowsDatasource = new MatTableDataSource<ProdReqColumnsWithCheckbox>([]);
+    this.productionRequestService.getSteps('62407').subscribe( resultResponse => {
+      this.productionRequests = resultResponse;
+      const existingSteps: string[] = [];
+      for (const slice of this.productionRequests.slices){
+        if (slice.steps.length > 0){
+          for (const step of slice.steps){
+            if (!existingSteps.includes(step.step_name)){
+              existingSteps.push(step.step_name);
+            }
+          }
+        }
+      }
+      const rows: ProdReqColumnsWithCheckbox[] = [];
+      let currentOffset = 0;
+      for (const slice of this.productionRequests.slices){
+        this.indexMap.set(slice.slice, currentOffset);
+        const renderedSlices = renderSlice(slice, existingSteps);
+        currentOffset += renderedSlices.length;
+        this.renderedSlicesMap.set(slice.slice, renderedSlices);
+        rows.push(...renderedSlices);
+      }
+            //bottom offset rows
+      const offsetRow: ProdReqColumnsWithCheckbox = {
+        checkbox: false,
+        columns: [],
+        sliceNumber: -9999,
+        indexInsideSlice: -9999
+      };
+      rows.push(offsetRow);
+      rows.push(offsetRow);
+      rows.push(offsetRow);
+      this.rowsDatasource.data = rows;
+    });
   }
 
-  createMockRows(): ColumnsWihtCheckbox[] {
-    const mockRows: ColumnsWihtCheckbox[] = [];
-    const maxColumns = 10;
-    for (let i = 0; i < 150000; i++) {
-      const columns: Column[] = [];
-      let usedColumns = 0;
-      for (let j = 0; j < 10; j++) {
-        let randomSpan = Math.floor(Math.random() * 4) + 1;
-        if (usedColumns + randomSpan > maxColumns) {
-          randomSpan = maxColumns - usedColumns;
-        }
-        if (randomSpan === 0) {
-          break;
-        }
 
-        const randomBackgroundColor = ["bg-red-200", "bg-green-200", "bg-blue-200", "bg-yellow-200"][Math.floor(Math.random() * 4)];
-        columns.push({
-          text: `Row ${i}, Col ${j}`,
-          span: randomSpan,
-          cssClasses: `border ${randomBackgroundColor}`,
 
-        });
-      }
-      const checkboxrandom = Math.floor(Math.random() * 3);
-      let checkbox = false;
-      if (checkboxrandom === 1) {
-        checkbox = true;
-      }
-      if (checkboxrandom === 0) {
-        checkbox = undefined;
-      }
-      mockRows.push({checkbox, columns, index: i});
+  handleColumnClick(sliceIndex: number, insideSliceIndex: number, colIndex: number) {
+    console.log(sliceIndex, insideSliceIndex, colIndex);
+    const cell = this.renderedSlicesMap.get(sliceIndex)[insideSliceIndex].columns[colIndex];
+    switch (cell.cellType){
+      case 'task':
+        this.router.navigate(['task', cell.value]);
+        break;
+      case 'step':
+        console.log('step');
+        break;
+      case 'input_data':
+        console.log('input_data');
+        break;
+      case 'dataset':
+        console.log('dataset');
+        break;
+      case 'comment':
+        console.log('comment');
+        break;
+      case 'status':
+        console.log('status');
+        break;
+      case 'empty':
+        console.log('empty');
+        break;
     }
-    return mockRows;
+  }
+
+  handleCheckboxChange(sliceIndex) {
+    console.log(sliceIndex);
   }
 
 
-  handleColumnClick(rowIndex, colIndex) {
-    console.log(rowIndex, colIndex);
-  }
-
-  handleCheckboxChange(rowIndex) {
-    console.log(rowIndex);
-  }
-
-  reShuffle() {
-    shuffle(this.rows);
-    this.rowsDatasource.data = this.rows;
-  }
 
   scrollTo(index: number) {
     if (index === -1){
-      this.scrollViewport.scrollToIndex(this.rows.length - 1);
+      this.scrollViewport.scrollToIndex(this.rowsDatasource.data.length - 1);
       return;
     }
     if (index === -2){
       this.scrollViewport.scrollTo({end: 0});
       return;
     }
-    this.scrollViewport.scrollToIndex(this.scrollIndex);
+    if (this.indexMap.has(index)){
+      this.scrollViewport.scrollToIndex(this.indexMap.get(index));
+    }
+
   }
 
   filterRows() {
-    const currentRealIndex = this.rowsDatasource.filteredData[this.currentIndex()].index;
-    this.rowsDatasource.filter = this.filterString.trim().toLowerCase();
-    console.log(currentRealIndex, this.rowsDatasource.filteredData.indexOf(this.rows[currentRealIndex]));
-    if (this.rowsDatasource.filteredData.indexOf(this.rows[currentRealIndex]) !== -1) {
-      this.scrollViewport.scrollToIndex(this.rowsDatasource.filteredData.indexOf(this.rows[currentRealIndex]));
+    const filteredRows: ProdReqColumnsWithCheckbox[] = [];
+    this.indexMap = new Map<number, number>();
+    for (const [sliceNumber, renderedSlices] of this.renderedSlicesMap.entries()){
+      if (renderedSlices !== undefined){
+        // Check if some column of the first row contains the filter string
+        if (renderedSlices[0].columns.some(column => column.text.toLowerCase().includes(this.filterString.toLowerCase()))) {
+          filteredRows.push(...renderedSlices);
+          this.indexMap.set(sliceNumber, filteredRows.length);
+        }
+      }
     }
+    this.rowsDatasource.data = filteredRows;
   }
 
   ngAfterViewInit(): void {
@@ -162,30 +191,117 @@ export class MainRequestComponent implements OnInit, AfterViewInit {
 
   }
 }
-export interface Column {
+export interface ProdReqColumn {
   text: string;        // The text content of the column cell
   span: number;        // The number of column spans (e.g., 1, 2, or 4)
   cssClasses: string;  // Tailwind CSS classes for styling
+  cellType: 'task'|'step'|'input_data'|'dataset'|'comment'|'status'|'empty'; // The type of the cell
+  value?: string; // The value of the cell (optional)
 }
 
-export interface ColumnsWihtCheckbox  {
+export interface ProdReqColumnsWithCheckbox {
   checkbox: boolean | undefined; // The checkbox value
-  index: number;
-  columns: Column[];             // The columns in the row
+  sliceNumber: number;
+  indexInsideSlice: number;
+  columns: ProdReqColumn[];             // The columns in the row
 }
 
-function shuffle(array) {
-  let currentIndex = array.length;
 
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
 
-    // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
 
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
+function renderSlice(slice: Slice, existingSteps: string[]): ProdReqColumnsWithCheckbox[] {
+    const columnNumbers = existingSteps.length + 2;
+    const rows: ProdReqColumnsWithCheckbox[] = [];
+    // first row
+    const firstRow: ProdReqColumnsWithCheckbox = {
+      checkbox: true,
+      columns: [],
+      sliceNumber: slice.slice,
+      indexInsideSlice: 0
+    };
+    const firstRowColumns: ProdReqColumn[] = [];
+    if (slice.input_data && !slice.dataset) {
+      firstRowColumns.push({text: slice.input_data, span: columnNumbers - 2, cssClasses: '', cellType: "input_data"});
+      firstRowColumns.push({text: '', span: 2, cssClasses: '', cellType: "empty"});
+    } else if (slice.dataset && !slice.input_data) {
+      firstRowColumns.push({text: slice.dataset, span: columnNumbers - 2, cssClasses: '', cellType: "dataset"});
+      firstRowColumns.push({text: '', span: 2, cssClasses: '', cellType: "empty"});
+    } else {
+      firstRowColumns.push({text: slice.input_data, span: columnNumbers / 2, cssClasses: '', cellType: "input_data"});
+      firstRowColumns.push({text: slice.dataset, span: columnNumbers - columnNumbers / 2, cssClasses: '', cellType: "dataset"});
+    }
+    firstRow.columns = firstRowColumns;
+    rows.push(firstRow);
+    // second row
+    const secondRow: ProdReqColumnsWithCheckbox = {
+      checkbox: false,
+      columns: [],
+      sliceNumber: slice.slice,
+      indexInsideSlice: 1
+    };
+    const secondRowColumns: ProdReqColumn[] = [];
+    secondRowColumns.push({text: slice.comment, span: columnNumbers - 2, cssClasses: '', cellType: "comment"});
+    secondRowColumns.push({text: 'events:', span: 1, cssClasses: '', cellType: "empty"});
+    secondRowColumns.push({text: slice.input_events.toString(), span: 1, cssClasses: '', cellType: "empty"});
+    secondRow.columns = secondRowColumns;
+    rows.push(secondRow);
+    // step tag row
+    const stepRow: ProdReqColumnsWithCheckbox = {
+      checkbox: false,
+      columns: [],
+      sliceNumber: slice.slice,
+      indexInsideSlice: 2
+    };
+    const stepRowColumns: ProdReqColumn[] = [];
+    const taskPerStep: ProductionTask[][] = new Array(existingSteps.length);
+    const stepsInSlice = slice.steps.length;
+    let maxTasksLength = 0;
+    for (let i = 0, j = 0; i < existingSteps.length; i++) {
+      if (j >= stepsInSlice) {
+        stepRowColumns.push({text: '', span: 1, cssClasses: '', cellType: "empty"});
+        taskPerStep[i] = [];
+        continue;
+      }
+      if (slice.steps[j].step_name === existingSteps[i]) {
+           stepRowColumns.push({text: slice.steps[j].ami_tag, span: 1, cssClasses: '', cellType: "step"});
+           taskPerStep[i] = slice.steps[j].tasks;
+           if (slice.steps[j].tasks.length > maxTasksLength) {
+             maxTasksLength = slice.steps[j].tasks.length;
+           }
+           j++;
+      } else {
+          stepRowColumns.push({text: '', span: 1, cssClasses: '', cellType: "empty"});
+          taskPerStep[i] = [];
+      }
+    }
+    stepRowColumns.push({text: 'Status 1', span: columnNumbers - existingSteps.length, cssClasses: '', cellType: "status"});
+    stepRow.columns = stepRowColumns;
+    rows.push(stepRow);
+    for (let i = 0; i < maxTasksLength; i++) {
+      const taskRow: ProdReqColumnsWithCheckbox = {
+        checkbox: false,
+        columns: [],
+        sliceNumber: slice.slice,
+        indexInsideSlice: 3 + i
+      };
+      const taskRowColumns: ProdReqColumn[] = [];
+      for (let j = 0; j < existingSteps.length; j++) {
+        if (taskPerStep[j][i]) {
+          taskRowColumns.push({text: taskPerStep[j][i].status.toString(), span: 1, cssClasses: '',
+            cellType: "task", value: taskPerStep[j][i].id.toString()});
+        } else {
+          taskRowColumns.push({text: '', span: 1, cssClasses: '', cellType: "empty"});
+        }
+      }
+      if (i === 0){
+        taskRowColumns.push({text: 'Status 2', span: columnNumbers - existingSteps.length, cssClasses: '', cellType: "status"});
+      } else {
+        taskRowColumns.push({text: '', span: columnNumbers - existingSteps.length, cssClasses: '', cellType: "empty"});
+      }
+      taskRow.columns = taskRowColumns;
+      rows.push(taskRow);
+
+
+    }
+    return rows;
 }
--- atlas/frontendjs/prodsysjs/src/app/production-request/pattern-selection/pattern-selection.component.ts
+++ atlas/frontendjs/prodsysjs/src/app/production-request/pattern-selection/pattern-selection.component.ts
@@ -1,6 +1,7 @@
 import {Component, computed, input, OnInit, signal} from '@angular/core';
-import {CampaignPattern, MCPattern} from "../production-request-models";
+import {CampaignPattern, MCPattern, PatternSteps} from "../production-request-models";
 import {toObservable} from "@angular/core/rxjs-interop";
+import {NgClass} from "@angular/common";
 
 interface CampaignPatternWithStyles {
   style: string;
@@ -9,13 +10,15 @@ interface CampaignPatternWithStyles {
 
 @Component({
   selector: 'app-pattern-selection',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './pattern-selection.component.html',
   styleUrl: './pattern-selection.component.css'
 })
-export class PatternSelectionComponent implements OnInit {
+export class PatternSelectionComponent {
 
-  patterns = input(undefined, {transform: this.prepareStyles});
+  patterns = input<CampaignPattern[]>([]);
 
   campaignColors = [
     "bg-red-200",
@@ -23,35 +26,26 @@ export class PatternSelectionComponent implements OnInit {
     "bg-blue-200",
     "bg-yellow-200"
   ];
-
-  styleReady = signal(false);
-
-  campaignsWithStyles = computed(() => this.patterns().campaignPatterns.map((campaign, index) => {
-    return {
-      ...campaign,
-       style: `col-span-${stepsInPattern(campaign.patterns)} font-bold text-gray-700 border-4 border-indigo-600 ${this.campaignColors[index % this.campaignColors.length]}`
-
-    };
-  }
-  ));
-inputObs$ = toObservable(this.patterns);
-ngOnInit(): void {
-  this.inputObs$.subscribe((campaigns) => {
-    if (campaigns.campaignPatterns.length > 0) {
-      this.styleReady.set(true);
-    }
+  stepArray = computed<PatternSteps[]>( () => {
+    return this.patterns().reduce((acc, campaign) => {
+      return acc.concat(campaign.patterns.reduce((acc2, pattern) => {
+        return acc2.concat(pattern.steps);
+      }, []));
+    }, []);
   });
+  patternArray = computed<MCPattern[]>( () => {
+    return this.patterns().reduce((acc, campaign) => {
+      return acc.concat(campaign.patterns);
+    }, []);
+  });
+
+    getGridTemplate(): string {
+      const totalColumns = this.stepArray().length;
+      return `repeat(${totalColumns}, 1fr)`;
+  }
+
+    getColorClass(index: number): string {
+    return this.campaignColors[index % this.campaignColors.length];
+  }
 }
 
-  prepareStyles(patterns: CampaignPattern[]| undefined): CampaignPatternWithStyles {
-    const totalSteps = patterns.reduce((acc, campaign) => acc +
-      stepsInPattern(campaign.patterns), 0);
-    return {
-      campaignPatterns: patterns,
-       style: `border-solid grid grid-cols-${totalSteps} cursor-pointer`
-    };
-  }
-}
-function  stepsInPattern(patterns: MCPattern[]): number {
-    return patterns.reduce((acc2, pattern) => acc2 + pattern.steps.length, 0);
-  }
