b7a5fd1be4ddf4c1bb79cb7b5b35674578d853f7
mishaborodin <borodin.misha@gmail.com>
2026-06-06 20:35:10 +0200

    Refactor DAOD extension interface

--- atlas/gpdeletion/views.py
+++ atlas/gpdeletion/views.py
@@ -40,7 +40,7 @@ _logger = logging.getLogger('prodtaskwebui')
 _jsonLogger = logging.getLogger('prodtask_ELK')
 
 
-FORMAT_BASES = ['BPHY', 'EGAM', 'EXOT', 'FTAG', 'HDBS', 'HIGG', 'HION', 'JETM', 'LCALO', 'LLP', 'LLJ', 'MUON', 'NCB',
+FORMAT_BASES = ['BPHY', 'EGAM', 'EXOT', 'FTAG', "IDTR", 'HDBS', 'HIGG', 'HION', 'JETM', 'LCALO', 'LLP', 'LLJ', 'MUON', 'NCB','PHYS',
                 'STDM', 'SUSY', 'TAUP', 'TCAL', 'TLA',  'TOPQ', 'TRIG', 'TRUTH']
 
 CP_FORMATS = ["FTAG", "EGAM", "MUON", "JETM", "TAUP", "IDTR", "TCAL", 'PHYS']
